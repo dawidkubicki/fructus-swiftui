ed31970573becc252c6b36ad8490b043adf8188d
Dawid Kubicki <dawid@pluscode.io>
2021-03-02 11:00:51 +0100

    init commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Dawid Kubicki on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
